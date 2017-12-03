59169e0ebbbf64ab540dc679a770ee5e5b0bcc02
disa6302 <disa6302@colorado.edu>
2017-12-02 17:12:30 -0700

    Tested FreeRTOS LED Blink Demo Application and UART Operation

---
+++ main.c
@@ -0,0 +1,109 @@
+/* FreeRTOS 8.2 Tiva Demo
+ *
+ * main.c
+ *
+ * Andy Kobyljanec
+ *
+ * This is a simple demonstration project of FreeRTOS 8.2 on the Tiva Launchpad
+ * EK-TM4C1294XL.  TivaWare driverlib sourcecode is included.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "main.h"
+#include "drivers/pinout.h"
+#include "utils/uartstdio.h"
+
+
+// TivaWare includes
+#include "driverlib/sysctl.h"
+#include "driverlib/debug.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+
+// FreeRTOS includes
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+
+// Demo Task declarations
+void demoLEDTask(void *pvParameters);
+void demoSerialTask(void *pvParameters);
+
+// Main function
+int main(void)
+{
+    // Initialize system clock to 120 MHz
+    uint32_t output_clock_rate_hz;
+    output_clock_rate_hz = ROM_SysCtlClockFreqSet(
+                               (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN |
+                                SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
+                               SYSTEM_CLOCK);
+    ASSERT(output_clock_rate_hz == SYSTEM_CLOCK);
+
+    // Initialize the GPIO pins for the Launchpad
+    PinoutSet(false, false);
+
+    // Create demo tasks
+    xTaskCreate(demoLEDTask, (const portCHAR *)"LEDs",
+                configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+    xTaskCreate(demoSerialTask, (const portCHAR *)"Serial",
+                configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+    return 0;
+}
+
+
+// Flash the LEDs on the launchpad
+void demoLEDTask(void *pvParameters)
+{
+    for (;;)
+    {
+        // Turn on LED 1
+        LEDWrite(0x0F, 0x01);
+        vTaskDelay(1000);
+
+        // Turn on LED 2
+        LEDWrite(0x0F, 0x02);
+        vTaskDelay(1000);
+
+        // Turn on LED 3
+        LEDWrite(0x0F, 0x04);
+        vTaskDelay(1000);
+
+        // Turn on LED 4
+        LEDWrite(0x0F, 0x08);
+        vTaskDelay(1000);
+    }
+}
+
+
+// Write text over the Stellaris debug interface UART port
+void demoSerialTask(void *pvParameters)
+{
+    // Set up the UART which is connected to the virtual COM port
+    UARTStdioConfig(0, 57600, SYSTEM_CLOCK);
+
+
+    for (;;)
+    {
+        UARTprintf("\r\nHello, world from FreeRTOS 9.0!");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+
+/*  ASSERT() Error function
+ *
+ *  failed ASSERTS() from driverlib/debug.h are executed in this function
+ */
+void __error__(char *pcFilename, uint32_t ui32Line)
+{
+    // Place a breakpoint here to capture errors until logging routine is finished
+    while (1)
+    {
+    }
+}
